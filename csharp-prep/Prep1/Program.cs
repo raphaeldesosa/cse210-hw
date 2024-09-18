1172697d12c807017d0909a2201c33cf12bbb489
raphaeldesosa <des22005@byui.edu>
2024-09-18 20:35:51 +0800

    finished prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Write your First Name: ");
+        string first = Console.ReadLine();
+
+        Console.Write("Write your Last Name: ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first}. ");
+
     }
 }
