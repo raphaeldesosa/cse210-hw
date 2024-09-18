ccd345ecb82cc167ff7ceab891e82e6ebf94c5f2
raphaeldesosa <des22005@byui.edu>
2024-09-18 21:05:44 +0800

    finished with prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string Grade = Console.ReadLine();
+        int number = int.Parse(Grade);
+
+        string Letter = "";
+
+        if (number >= 90)
+        {
+            Letter = "A";
+        }
+        else if (number >= 80)
+        {
+            Letter = "B";
+        }
+        else if (number >= 70)
+        {
+            Letter = "C";
+        }
+        else if (number >= 60)
+        {
+            Letter = "D";
+        }
+        else
+        {
+            Letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {Letter}");
+
+        if (number > 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try harder next time");
+        }
+
+
+        
     }
 }
