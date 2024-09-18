b902f07d5b3e3f9479b9722c843b392ffc9309ae
raphaeldesosa <des22005@byui.edu>
2024-09-18 21:33:01 +0800

    Finished with prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random_generator = new Random();
+        int magic_number = random_generator.Next(1, 100);
+
+        int guess = -1;
+
+        while (guess != magic_number)
+        {
+            Console.Write("What's the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic_number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }    
     }
 }
