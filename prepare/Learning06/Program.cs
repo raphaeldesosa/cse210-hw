43c3fb171bd90fda3a09c9372226299c5ff56522
raphaeldesosa <des22005@byui.edu>
2024-10-24 22:37:23 +0800

    done with learning 06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Diagnostics;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square(6, "brown");
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle(5, 10, "red");
+        shapes.Add(s2);
+
+        Circle s3 = new Circle(8, "yellow");
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
